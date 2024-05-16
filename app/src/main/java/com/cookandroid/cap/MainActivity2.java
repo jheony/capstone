fc45f531c3796f55bf098b488afda008fad0e7a7
JiHyeon Yun <heon__y@naver.com>
2024-05-16 13:31:10 +0900

    list

--- app/src/main/java/com/cookandroid/cap/MainActivity2.java
+++ app/src/main/java/com/cookandroid/cap/MainActivity2.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -32,24 +36,23 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value = snapshot.getValue(String.class);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+        setTitle("리스트뷰 테스트");
+
+        final String[] mid = {"히어로즈","23시","로스트","졸리다"};
+        ListView list = (ListView) findViewById(R.id.listview_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mid);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),mid[position],Toast.LENGTH_SHORT).show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -57,59 +60,4 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
     }
-    private class ListView_Item {
-        private String title;
-
-        public ListView_Item(String title){
-            this.title = title;
-        }
-        public String getTitle(){
-            return title;
-        }
-    }
-
-    private class ListView_Adapter extends BaseAdapter implements com.cookandroid.cap.ListView_Adapter {
-        List<ListView_Item> items = null;
-        Context context;
-        public ListView_Adapter(Context context, List<ListView_Item> items){
-            this.items = items;
-            this.context = context;
-        }
-
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public ListView_Item getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // Infalter 구현 방법 1
-            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view = layoutInflater.inflate(R.layout.activity_main2, parent, false);
-
-            // ListView의 Item을 구성하는 뷰 연결
-            TextView number = view.findViewById(R.id.listitem_number);
-            TextView title = view.findViewById(R.id.listitem_title);
-
-            // ListView의 Item을 구성하는 뷰 세팅
-            ListView_Item item = items.get(position);
-            number.setText(String.valueOf(position+1));		// 해당위치 +1 설정, 배열순으로 0부터 시작
-            title.setText(item.getTitle());					// item 객체 내용을 가져와 세팅
-
-            // 설정한 view를 반환해줘야 함
-            return view;
-        }
-
-
-    }
-}
+  }
